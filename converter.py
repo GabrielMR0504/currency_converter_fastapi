8fd4d5de07c859c9c42607de731036b7cc6ff364
GabrielMR0504 <gabrielmadermr@gmail.com>
2024-05-11 14:02:53 -0300

    converter async

--- converter.py
+++ converter.py
@@ -2,6 +2,7 @@ from environments import ALPHAVANTAGE_KEY
 import requests
 from fastapi import HTTPException
 import traceback
+import aiohttp
 
 
 def sync_converter(from_currency: str, to_currency: str, price: float):
@@ -16,12 +17,33 @@ def sync_converter(from_currency: str, to_currency: str, price: float):
     data = response.json()
 
     if "Realtime Currency Exchange Rate" not in data:
-        print(url)
-        print(data)
         raise HTTPException(
-            status_code=400, detail='Realtime Currency Exchange Rate not in response')
+            status_code=400, detail=f'Realtime Currency Exchange Rate not in response {data}')
 
     exchange_rate = float(
         data['Realtime Currency Exchange Rate']['5. Exchange Rate'])
 
     return price * exchange_rate
+
+
+async def async_converter(from_currency: str, to_currency: str, price: float):
+    url = f'https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency={
+        from_currency}&to_currency={to_currency}&apikey={ALPHAVANTAGE_KEY}'
+
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url) as response:
+                data = await response.json()
+
+    except Exception:
+        raise HTTPException(
+            status_code=400, detail=traceback.format_exc(limit=1))
+
+    if "Realtime Currency Exchange Rate" not in data:
+        raise HTTPException(
+            status_code=400, detail=f'Realtime Currency Exchange Rate not in response {data}')
+
+    exchange_rate = float(
+        data['Realtime Currency Exchange Rate']['5. Exchange Rate'])
+
+    return {to_currency: price * exchange_rate}
